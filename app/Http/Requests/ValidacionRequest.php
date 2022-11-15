4bc0bf580f8d821b3bca9c5c5a2cfd88f61785e1
danielaguileta10 <daniel.aguileta@unah.hn>
2022-11-15 16:25:26 -0600

    validacion

---
+++ app/Http/Requests/ValidacionRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ValidacionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'cantidad' => 'required|numeric',
+              
+    ];
+    }
+
+    public function messages()
+    {
+        return[
+            /* Validacion campo NOMBRE CLIENTE */
+             'cantidad.required' => 'El campo cantidad  es requerido. No puede estar vacio.' ,
+              'cantidad.numeric' => 'El campo cantidad solo puede contener numeros.' ,
+              
+        ];
+    }
+}
