988498aaa31f8049dbc7e3838c509b9c7144172d
danielaguileta10 <daniel.aguileta@unah.hn>
2022-11-17 09:56:55 -0600

    contrller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,9 +11,6 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     
-    public function show()
-{
-    return view('profile.show');
-}
+
 }
 
