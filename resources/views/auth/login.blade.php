e0f39b23d4ef967d61127dd914726e1646f282e6
danielaguileta10 <daniel.aguileta@unah.hn>
2022-11-16 18:11:49 -0600

    borrar registro personas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,5 @@
 <x-guest-layout>
-<div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">REGISTRAR</a>
-</div>
+
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
