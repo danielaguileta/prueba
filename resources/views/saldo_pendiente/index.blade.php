9ac5721ec4a40089e5634c9999ba8e2c606da96c
danielaguileta10 <daniel.aguileta@unah.hn>
2022-11-17 18:43:02 -0600

    vista index

--- resources/views/saldo_pendiente/index.blade.php
+++ resources/views/saldo_pendiente/index.blade.php
@@ -17,9 +17,13 @@
             <div class="card-content">
 
             </div>
-
+<style>
+    table.dataTable thead tr {
+  background-color: black;
+}
+</style>
             <table id="clientes" class="display responsive nowrap" style="width:100%">
-                <thead class="thead-dark">
+                <thead class="text-white">
                     <tr>
                         <th width="2%">FECHA</th>
                         <th width="2%">SALDO</th>
